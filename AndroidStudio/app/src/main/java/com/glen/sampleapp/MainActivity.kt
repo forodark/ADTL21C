46077ce2708216080f5f17e5450e05a00ae826ce
Glen <138078342+forodark@users.noreply.github.com>
2023-11-06 23:11:56 +0800

    android studio test apps

---
+++ AndroidStudio/app/src/main/java/com/glen/sampleapp/MainActivity.kt
@@ -0,0 +1,40 @@
+import android.os.Bundle
+import android.view.Menu
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.glen.sampleapp.ui.theme.SampleAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SampleAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Your Compose UI goes here
+                    MyComposable()
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.example_menu, menu)
+        return true
+    }
+
+    // Define your Compose UI here
+    @Composable
+    fun MyComposable() {
+        // Your UI components go here
+        Text(text = "Hello, World!")
+    }
+}
