72f8acda55f4eb7685f1dda21672817fd03fdfa4
Glen <138078342+forodark@users.noreply.github.com>
2023-11-10 11:22:14 +0800

    added some upgrades

--- CookieClicker/app/src/main/java/com/glen/cookieclicker/MainActivity.kt
+++ CookieClicker/app/src/main/java/com/glen/cookieclicker/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.os.SystemClock
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.Math.pow
 
 class MainActivity : AppCompatActivity() {
-    var cookies = 0;
+    var cookies = 0
+    var bakery_count = 0
+    var upgrade_count = 0
 
     private var handler: Handler? = null
     private var timer: Runnable? = null
@@ -19,41 +23,66 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Create a handler to post the toast message
+        // create a looper that adds 10 cookies every 2000 milliseconds
         handler = Handler()
-
-        // Create a runnable to update the timer
-        timer = Runnable {
-            // Get the current time in milliseconds
-            val time = SystemClock.elapsedRealtime()
-
-            // Update the timer text
-            // ...
-
-            // Display the toast message every 4500 milliseconds
-            if (time % 2000 == 0L) {
-                addCookie(10)
-                Toast.makeText(this, "+10", Toast.LENGTH_SHORT).show()
+        timer = object : Runnable {
+            override fun run() {
+                if(bakery_count>0) {
+                    addCookie(bakery_count * 5)
+                }
+                handler!!.postDelayed(this, 1000)
             }
-
-            // Schedule the next timer update
-            handler?.postDelayed(timer!!, 1)
         }
-
-        // Start the timer
-        handler?.post(timer!!)
+        handler!!.post(timer!!)
 
 
 
         val main_cookie: ImageButton = findViewById(R.id.cookie)
+        val add_bakery: ImageButton = findViewById(R.id.bakery_button)
+        val upgrade: Button = findViewById(R.id.upgrade)
 
         main_cookie.setOnClickListener {
-            addCookie()
+
+            addCookie((1*pow(2.0, upgrade_count.toDouble())).toInt())
         }
+
+        add_bakery.setOnClickListener {
+            val bakery_cost: Int = 100*(bakery_count+1)
+            if(cookies >= bakery_cost) {
+                addCookie(-1*bakery_cost)
+                addBakery()
+            }
+            else {
+                Toast.makeText(this, "Not enough cookies!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        upgrade.setOnClickListener {
+
+            if(cookies >= 20) {
+                addCookie(-20)
+                addUpgrade()
+            }
+            else {
+                Toast.makeText(this, "Not enough cookies!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 
     fun addCookie(count: Int = 1) {
         cookies += count
         findViewById<TextView>(R.id.cookie_count).text = "Cookies: $cookies"
+    }
+
+    fun addBakery(count: Int = 1) {
+        bakery_count += count
+        findViewById<TextView>(R.id.bakery_count).text = "$bakery_count"
+    }
+
+    fun addUpgrade(count: Int = 1) {
+        upgrade_count += count
+        findViewById<TextView>(R.id.level).text = "Lvl $upgrade_count"
     }
 }
